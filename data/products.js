ade502313bf741ebe58cd6e61591431b70428a13
YUNUS.A <mohammedyunus1847@gmail.com>
2026-08-28 22:34:54 +0530

    covert the products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,32 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  keywords;
+
+  constructor(productDetails){
+    this.id=productDetails.id;
+    this.image=productDetails.image;
+    this.name=productDetails.name;
+    this.rating=productDetails.rating;
+    this.priceCents=productDetails.priceCents;
+    this.keywords=productDetails.keywords
+
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars*10}.png`;
+  };
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+
+}
+
+
 export function getProduct(productId){
   let matchingProduct;
         Products.forEach((product)=>{
@@ -8,6 +37,9 @@ export function getProduct(productId){
         });
   return matchingProduct
 }
+
+
+
 
 export const Products = [
   {
@@ -700,4 +732,8 @@ export const Products = [
     },
     priceCents:2367
   }
-];
+].map((productDetails)=>{
+  return new Product(productDetails);
+});
+
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {rendercheckoutHeader} from './checkout/checkoutHeader.js';
 import {loadFromStorage } from '../data/cart.js';
-import '../data/cart-class.js'
+//import '../data/cart-class.js'
 
 loadFromStorage();
 rendercheckoutHeader();
